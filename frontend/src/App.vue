439abdfc42d4c4ea4ea6f8ddbb8f22c6484ea2b2
Qtekfun <qtekfun@gmail.com>
2025-08-17 21:17:02 +0200

    feat: cleanup template

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,7 +8,9 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
+      <div class="bg-blue-500">
       <HelloWorld msg="You did it!" />
+      </div>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
